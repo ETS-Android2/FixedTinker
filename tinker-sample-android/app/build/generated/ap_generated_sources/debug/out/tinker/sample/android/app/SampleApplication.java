294dfbd92cb265a4bdb79a5d96ba4c7ae6fa6c40
zhangyan <2166134687@qq.com>
2021-09-08 16:09:59 +0800

    update

---
+++ tinker-sample-android/app/build/generated/ap_generated_sources/debug/out/tinker/sample/android/app/SampleApplication.java
@@ -0,0 +1,16 @@
+package tinker.sample.android.app;
+
+import com.tencent.tinker.loader.app.TinkerApplication;
+
+/**
+ *
+ * Generated application for tinker life cycle
+ *
+ */
+public class SampleApplication extends TinkerApplication {
+
+    public SampleApplication() {
+        super(15, "tinker.sample.android.app.SampleApplicationLike", "com.tencent.tinker.loader.TinkerLoader", false, false);
+    }
+
+}
